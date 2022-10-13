8883df47fe54b0891dac7390dad2c8953924720a
Mikael Akpala <mikaelakpala@The-Hidden-Genius-Project-9.local>
2022-10-12 18:25:03 -0700

    first commit

---
+++ terminal-lingo.py
@@ -0,0 +1,19 @@
+def terminal_command():
+  command = input("Type a command for a defition: ")
+  if (command == "touch"):
+    print("\n")
+    print("this command is used to create files")
+  if (command == "cd"):
+    print("\n")
+    print("this command is used to create files")
+  if (command == "ls"):
+    print("\n")
+    print("this command is used to create files")
+  if (command == "mkdir"):
+    print("\n")
+    print("this command is used to create files")
+  if (command == "cat"):
+    print("\n")
+    print("this command is used to create files")
+terminal_command()
+
